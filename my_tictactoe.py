1b7a5d0ace8fbd8e4a5acb276d05b3b95315b0be
Zeus-Max-Code <hazardos10@gmail.com>
2022-04-25 19:22:30 +0000

    create the function(code skeleton)

---
+++ my_tictactoe.py
@@ -0,0 +1,35 @@
+'''
+Game of Tic-Tac-Toe.
+Author: Isaac Asiedu
+'''
+
+def main():
+    pass
+
+
+def make_a_board():
+    pass
+
+
+def board_display():
+    pass
+
+
+def make_the_winner():
+    pass
+
+
+def is_a_draw():
+    pass
+
+
+def make_move():
+    pass
+
+def next_player():
+    pass
+
+
+
+if __name__ == "__main__":
+    main
